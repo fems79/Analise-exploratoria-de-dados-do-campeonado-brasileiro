066ff99c9d34809a7144ef03289bb777a733c616
Felipe Ramos Santana <felipe.ramos@dcomp.ufs.br>
2025-08-27 21:38:07 -0300

    Update Partida.java
    Atualização para adequação com as atualizações

--- AnaliseJava/Partida.java
+++ AnaliseJava/Partida.java
@@ -1,4 +1,3 @@
-package AnaliseJava.Dados;
 public class Partida {
     private final String mandante;
     private final String visitante;
@@ -12,13 +11,14 @@ public class Partida {
         this.golsVisitante = golsVisitante;
     }
 
-    public String getVencedor() {
-        if (golsMandante > golsVisitante) {
-            return mandante;
-        } else if (golsVisitante > golsMandante) {
-            return visitante;
-        } else {
-            return "Empate";
-        }
+    public String getMandante() { return mandante; }
+    public String getVisitante() { return visitante; }
+    public int getGolsMandante() { return golsMandante; }
+    public int getGolsVisitante() { return golsVisitante; }
+
+    public int[] getPontos() {
+        if (golsMandante > golsVisitante) return new int[]{3, 0};
+        else if (golsVisitante > golsMandante) return new int[]{0, 3};
+        else return new int[]{1, 1};
     }
 }
